468cae21c4b9310a0a943910735a1c69f307551b
zeinrivo <110659540+zeinrivo@users.noreply.github.com>
2023-05-23 19:20:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,8 @@ if selected == "Home":
 
     image1 = Image.open('random-forest.png')
     image2 = Image.open('one-water.png')
-    st.image(image2).thumbnail((400,400))
+    image2.thumbnail((400,400))
+    st.image(image2)
     st.image(image1)
     
     st.markdown(
