972bfe3c55d603d992c851d6852ad8067f6199f4
zeinrivo <110659540+zeinrivo@users.noreply.github.com>
2023-05-23 20:04:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ if selected == "Model":
     )
     st.markdown(
     """
-    - [Google Colaboratory](https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html)
+    - [Google Colaboratory](https://drive.google.com/drive/folders/1IsV7ZTiEyOao3RoPiY-TympCGeiKOkPU?usp=share_link)
     """
     )
 
